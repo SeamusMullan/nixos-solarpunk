d02a708578499283ab762c620eae778d3c9264a0
Seamus Mullan <seamusmullan2023@gmail.com>
2025-09-27 11:30:23 +0100

    update again desktop.nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }:
 
 {
-  # Enable X11 + Wayland display managers
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
 
-  # Window managers
   services.xserver.windowManager.i3.enable = true;
   programs.sway.enable = true;
 
-  # Monitor auto-layout
+  # autorandr for X11
   services.autorandr = {
     enable = true;
     profiles = {
@@ -31,6 +29,7 @@
     };
   };
 
+  # kanshi for Wayland
   services.kanshi = {
     enable = true;
     profiles = {
@@ -52,10 +51,10 @@
   };
 
   environment.systemPackages = with pkgs; [
-    feh        # wallpaper for i3
-    swaybg     # wallpaper for Sway
-    nwg-look   # GTK theming
-    qt6ct      # Qt theming
+    feh
+    swaybg
+    nwg-look
+    qt6ct
   ];
 }
 
